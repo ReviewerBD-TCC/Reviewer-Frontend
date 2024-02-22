8e26422929b36ed48077f0929b3b5aca26f66a92
vicorsi <victorhugocorsi@gmail.com>
2024-02-22 11:14:13 -0300

    init: Project base

--- reviewer-frontend/src/main.tsx
+++ reviewer-frontend/src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Menu from './pages/Menu/Menu'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/*Page Incial */}
+    <Menu/>
   </React.StrictMode>,
 )
--- reviewer-frontend/tailwind.config.js
+++ reviewer-frontend/tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./index.html",
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+    //   colors: {
+    //     'bosch-white': '#FFFFFF',
+    //     'bosch-black': '#000000',
+    //     'bosch-gray-50': '#71767C',
+    //     'bosch-red-50': '#ED0007',
+    //     'bosch-purple-40': '#9E2896',
+    //     'bosch-blue-50': '#007BC0',
+    //     'bosch-turquoise-50': '#18837E',
+    //     'bosch-green-50': '#00884A',
+    //     'bosch-yellow-85': '#FFCF00',
+      }
+    },
+  
   plugins: [],
 }
 
