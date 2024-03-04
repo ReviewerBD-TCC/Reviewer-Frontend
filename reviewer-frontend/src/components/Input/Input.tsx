15ab18fafcdcd4b7cc8c1dc48c71db588c993d8a
vicorsi <victorhugocorsi@gmail.com>
2024-03-04 16:26:23 -0300

    fix: Updated question title

--- reviewer-frontend/src/components/Input/Input.tsx
+++ reviewer-frontend/src/components/Input/Input.tsx
@@ -7,8 +7,7 @@ interface InputProps {
 const Input: React.FC<InputProps> = () => {
   return (
         <div className='bg-[#D0D4D8]  w-[100%] h-[40px] border-b-[1px] border-black'>
-            <Question title='Lorem ipsum dolor sit amet. Eos quia obcaecati aut totam provident 
-            sit totam obcaecati aut esse expedita ut numquam incidunt. ' isActive="false" />
+            <Question title='Lorem ipsum dolor sit amet. ' isActive="false" />
         </div>
   )
 }
