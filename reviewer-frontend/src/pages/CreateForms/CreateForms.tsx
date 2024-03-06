1445b841f5a05f20c8d1b704ca2d3c834e451eef
jzsantana <juliass7187@gmail.com>
2024-03-06 16:35:37 -0300

    feat: New CSS

--- reviewer-frontend/src/pages/CreateForms/CreateForms.tsx
+++ reviewer-frontend/src/pages/CreateForms/CreateForms.tsx
@@ -6,37 +6,45 @@ function CreateForms() {
     const yearsOptions =
      '[{"label":"2024","value":"2024"},{"label":"2025","value":"2025"}]'
 
+     const questionsOptions =
+     '[{"label":"O que essa pessoa já faz, mas poderia estar fazendo mais? Se possível, por gentileza, forneça exemplos.","value":"2024"},{"label":"O que essa pessoa já faz, mas poderia estar fazendo mais? Se possível, por gentileza, forneça exemplos.","value":"2025"}]'
+
   return (
-    <div className="h-screen">
+    <>
         <Header/>
         <div className={`bg-[#D0D0D0] w-full overflow-hidden flex justify-center items-center`}>
             <div className="bg-boschWhite w-[90%] h-screen flex items-center justify-center">
-                <div className="w-[90%]  flex flex-col gap-12">
+                <div className="w-[90%] h-auto flex flex-col gap-9">
                     <h1 className="text-3xl font-bold">Criação de formulário</h1>
                     <div className="flex justify-end">
-                        <SparkButton text="Personalizar"/>
+                        <SparkButton type="button" text="Personalizar"/>
                     </div>
-                    <div className="flex w-full justify-between items-center">
+                    <div className="w-full flex flex-row justify-between">
                         <div className="w-[70%]">
-                            <SparkTextfield label="Títu=lo do feedback" placeholder="Feedback" />
+                            <SparkTextfield label="Título do feedback" placeholder="Feedback" />
                         </div>
                         <div className="w-[20%]">
                             <SparkDropdown label="Ano" options={yearsOptions} whenChange={()=>{}}/>
                         </div>
                     </div>
-                    <div className="bg-[#f1f1f1] w-[95%] flex justify-center items-center  ">
-                        <SparkDropdown label="Pergunta" options={yearsOptions} whenChange={()=>{}}/>
+
+                    <div className="w-full h-[125px] flex justify-center items-center ">
+                        <div className="bg-[#F1F1F1] w-[90%] h-full flex justify-center items-center">
+                            <SparkDropdown label="Pergunta" options={questionsOptions} whenChange={()=>{}} />
+                        </div>
                     </div>
-                    <div className="">
+                    
+                    <div className="w-full flex justify-between items-center">
                         <SparkButton text="Adicionar pergunta" icon="add"/>
+                        <SparkButton text="Finalizar" />
                     </div>
-                    <div className="flex justify-end">
-                        <SparkButton text="Finalizar"/>
+                    <div className="w-full h-auto flex justify-end">
+                        
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </>
   )
 }
 
