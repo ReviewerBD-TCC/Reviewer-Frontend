f9ee15e1996ccfaf78e5ef8bd5c365e9d6f8c873
vicorsi <victorhugocorsi@gmail.com>
2024-03-11 16:38:21 -0300

    fix: Api questions

--- reviewer-frontend/src/components/Input/Input.tsx
+++ reviewer-frontend/src/components/Input/Input.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Question from '../Question/Question'
 
 interface InputProps {
+  title:string,
+  isActive:boolean,
 }
 
 const Input: React.FC<InputProps> = () => {
   return (
         <div className='bg-[#D0D4D8]  w-[100%] h-[40px] border-b-[1px] border-black'>
-            <Question title='Lorem ipsum dolor sit amet. ' isActive="false" />
+            <Question title='' isActive="true" />
         </div>
   )
 }
--- reviewer-frontend/src/pages/QuestionDb/QuestionDb.tsx
+++ reviewer-frontend/src/pages/QuestionDb/QuestionDb.tsx
@@ -4,10 +4,82 @@ import Input from "../../components/Input/Input"
 import useModal from "../../hooks/useModal";
 import Modal from "../../components/Modal/Modal";
 
+import api from "../../services/Api/Api";
+
+import { AxiosResponse } from 'axios'
+
+import { useForm } from 'react-hook-form'
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import  z  from 'zod'
+import { useEffect } from "react";
+
+
+const schema = z.object({
+    question: z.string(),
+    active: z.boolean().default(true),
+});
+
+type QuestionProps = z.infer<typeof schema>;
+
 function QuestionDb() {
 
   const { isOpen, toggle } = useModal();
 
+  
+const schema = z.object({
+  question: z.string(),
+  active: z.boolean().default(true),
+});
+
+type QuestionProps = z.infer<typeof schema>;
+
+function QuestionDb() {
+  const { isOpen, toggle } = useModal();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { isValid },
+  } = useForm<QuestionProps>({
+    defaultValues: { question: '', active: true },
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(schema),
+  });
+
+  const handleQuestion = async (props: QuestionProps) => {
+    try {
+      const response: AxiosResponse<QuestionProps> = await api.get('question', {
+        params: {
+          question: props.question,
+          active: props.active,
+        },
+      });
+      console.log(response.data.question);
+    } catch (error) {
+      console.error(error as Error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: AxiosResponse<QuestionProps> = await api.get('question', {
+          params: {
+            question: '',
+            active: true,
+          },
+        });
+        console.log(response.data.question);
+      } catch (error) {
+        console.error(error as Error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+}
 
   return (
     <div className="h-screen">
@@ -23,13 +95,11 @@ function QuestionDb() {
                       </SparkNotification>
                     </div>
                     <div className="w-[100%]">
-                      <Input/>
+                      <Input title="WADW" isActive={false} />
                     </div>
-                    
                 </div>
-                
                 <div className="2xl:w-[100%] flex flex-col gap-8 justify-end items-end lg:w-[90%]">
-                    <SparkButton text="Adicionar pergunta" customWidth="12rem" onClick={toggle}/>
+                    <SparkButton text="Adicionar pergunta" customWidth="12rem" onClick={()=>{}}/>
                     <Modal title="Criação de pergunta" isOpen={isOpen} toggle={toggle}/>
                 </div>
             </div>
