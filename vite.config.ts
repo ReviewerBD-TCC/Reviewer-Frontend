bbcc3129045d1d5ca1344b8d9e9c88fe65de611a
jzsantana <juliass7187@gmail.com>
2024-03-20 08:28:39 -0300

    build: Config files

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '10.234.86.79',
+    port: 5173,
+  }
+})
