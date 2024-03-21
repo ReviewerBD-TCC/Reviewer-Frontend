b05add002d9be7f297f233d4977ef60e642cf161
vicorsi <victorhugocorsi@gmail.com>
2024-03-21 11:18:15 -0300

    feat: Spark toggle put value

--- reviewer-frontend/src/components/Question/Question.tsx
+++ reviewer-frontend/src/components/Question/Question.tsx
@@ -1,6 +1,9 @@
 import { SparkToggle } from '@bosch-web-dds/spark-ui-react';
 import Modal from '../Modal/Modal';
 import useModal from '../../hooks/useModal';
+import { AxiosResponse } from 'axios'
+import api from '../../services/Api/Api';
+
 
 interface QuestionProps{
     titlePt: string;
@@ -12,6 +15,30 @@ interface QuestionProps{
 const Question: React.FC<QuestionProps> = (props) => {
 
     const { isOpen, toggle } = useModal();
+    
+    const id = props.id
+    const token = localStorage.getItem('token');
+
+    
+    async function updateToggle(props :QuestionProps) {
+
+
+        try{
+            const response: AxiosResponse = await api.put(
+                `question/${id}`,
+                {
+                    active: props.isActive,
+                },{
+                    headers: {
+                        'Authorization' : `Bearer ${token}`
+                    }
+                }
+            );
+            console.log(response.data.question);
+        }catch(error){
+            console.log(error)
+        }
+    }
 
     return (
         <div className='p-2 flex justify-between items-center'>
@@ -20,7 +47,7 @@ const Question: React.FC<QuestionProps> = (props) => {
             </button>
             <Modal title='Editor de pergunta' titlePtValue={props.titlePt} titleEnValue={props.titleEn} id={props.id} activeValue={props.isActive} isOpen={isOpen} toggle={toggle}/>
             <div>
-                <SparkToggle guid="spark-toggle-right-label" selected={props.isActive} disabled={false} whenChange={()=>{}}/>
+                <SparkToggle guid="spark-toggle-right-label" selected={props.isActive} disabled={false} whenChange={()=>{updateToggle}}/>
             </div>
     </div>
   )
