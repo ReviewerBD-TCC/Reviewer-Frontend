0ff0a2ca247ea98b0fea730937ef4afed68973f7
jzsantana <juliass7187@gmail.com>
2024-03-25 16:42:41 -0300

    feat: create select component

--- reviewer-frontend/src/components/Select/Select.tsx
+++ reviewer-frontend/src/components/Select/Select.tsx
@@ -1,6 +1,5 @@
 import React, { SelectHTMLAttributes } from 'react'
 
-
 export interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement | HTMLInputElement>{
   labelText:string;
   options?:Array<string>;
@@ -11,10 +10,10 @@ export const Select:React.FC<SelectProps> = ({labelText, options = [], value}, .
   return (
     <div className='w-auto w-max-full bg-boschLightGray flex flex-col relative h-12  ' >
         <label className='mt-1 mr-4 ml-4 mb-auto text-xs'>{labelText}</label>
-        <select value={value} {...rest} className='w-max-full h-12 outline-none appearance-none pr-11 pl-4 w-full bg-transparent text-[13.5px] overflow-hidden '>
+        <select value={value} {...rest} className='w-max-full h-12 outline-none appearance-none pr-11 pl-4 w-full bg-transparent text-[13.5px] overflow-hidden'>
           {
             options.map((opcao, index) =>(
-              <option key={index} value={opcao} className='min-h-3 whitespace-nowrap ' >{opcao}</option>
+              <option key={index} value={opcao} className='min-h-3 whitespace-nowrap'>{opcao}</option>
             ))
           }
         </select>
