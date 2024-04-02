5b309f40d1aac4e760b5e40dfca6505c95162dcc
vicorsi <victorhugocorsi@gmail.com>
2024-04-02 14:48:58 -0300

    fix: Question database services

--- reviewer-frontend/src/pages/QuestionDb/QuestionDb.tsx
+++ reviewer-frontend/src/pages/QuestionDb/QuestionDb.tsx
@@ -1,38 +1,29 @@
+import { Header } from "components";
 import { SparkButton, SparkNotification, SparkActivityIndicator} from "@bosch-web-dds/spark-ui-react"
+import { Input } from "components";
 import useModal from "../../hooks/useModal";
-import { ModalAdd, Input, Header } from "../../components/index";
+import Modal from "../../components/Modal/ModalAdd";
 import error404 from "../../assets/images/404.png";
 
 import api from "../../api/Api";
 
 import { useQuery } from "react-query";
-
-interface QuestionProps{
-  questionPt?: string,
-  questionEn?: string,
-  active?: boolean,
-  id?: number,
-}
+import { QuestionProps } from "interfaces/Question";
+import { QuestionService } from "services/QuestionService";
 
 function QuestionDb(props: QuestionProps) {
   const { isOpen, toggle } = useModal();
   const token = localStorage.getItem('token');
 
-  const { data: responseList = [], isLoading, error } = useQuery("question", async () => {
-    const response = await api.get('question', {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-    return response.data;
-  },)
+  
 
+  const { data: responseList = [], isLoading, error } = useQuery("questions", () => QuestionService.useQuestions(token));
 
   return (
     <div className="h-screen">
         <Header/>
-        <div className={`bg-[#D0D0D0] w-full h-full overflow-hidden flex justify-center items-center`}>
-          <div className="bg-boschWhite w-[90%] h-auto flex items-center justify-center">
+        <div className={`bg-[#D0D0D0] w-full overflow-hidden flex justify-center items-center`}>
+          <div className="bg-boschWhite w-[90%] h-screen flex items-center justify-center">
             <div className="w-[1234px] h-[729px] flex flex-col justify-center items-center gap-8">
                 <div className="2xl:w-[100%] flex flex-col justify-center items-start gap-2 lg:w-[90%]">
                     <h1 className="font-bold text-4xl text-start w-full ">Banco de perguntas</h1>
@@ -56,7 +47,7 @@ function QuestionDb(props: QuestionProps) {
                 </div>
                 <div className="2xl:w-[100%] flex flex-col gap-8 justify-end items-end lg:w-[90%]">
                     <SparkButton text="Adicionar pergunta" customWidth="12rem" onClick={toggle}/>
-                    <ModalAdd title="Criação de pergunta" id={props.id} activeValue={props.active} titlePtValue={props.questionPt} titleEnValue={props.questionEn} isOpen={isOpen} toggle={toggle}/>
+                    <Modal title="Criação de pergunta" id={props.id} activeValue={props.active} titlePtValue={props.questionPt} titleEnValue={props.questionEn} isOpen={isOpen} toggle={toggle}/>
                 </div>
             </div>
           </div>
