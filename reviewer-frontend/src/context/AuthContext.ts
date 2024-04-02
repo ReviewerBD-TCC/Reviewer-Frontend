a46eaa189887561ac5cd58ef7196e3820786d72d
vicorsi <victorhugocorsi@gmail.com>
2024-04-02 16:49:22 -0300

    feat: AuthContext and useContext

--- reviewer-frontend/src/context/AuthContext.ts
+++ reviewer-frontend/src/context/AuthContext.ts
@@ -1,3 +1,47 @@
-import { createContext, useContext, useState } from "react"; 
+import { createContext, useContext, useState, ReactNode } from "react";
+import { UserService } from '../services/UserService';
+import { UserData } from '../interfaces/CreateUser'
 
-import { UserService }
+interface AuthContextType {
+  setDetailsUser: (values: UserData) => void;
+  accessToken: string | null;
+  setAccessToken: (token: string | null) => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [user, setUser] = useState<UserData | undefined>();
+
+  const updateUser = async () => {
+    if (accessToken) {
+      try {
+        const response = await UserService.userDetails(accessToken);
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error updating user:", error);
+      }
+    }
+  };
+
+  function setDetailsUser(values: UserData) {
+    setUser(values);
+  }
+
+  return (
+    <AuthContext.Provider value={{ setDetailsUser, accessToken, setAccessToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
