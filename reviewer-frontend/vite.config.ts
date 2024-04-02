7245cdbe1a8f304e4e205fa61bb603773b725862
vicorsi <victorhugocorsi@gmail.com>
2024-04-02 14:50:39 -0300

    fix: Vite ip config

--- reviewer-frontend/vite.config.ts
+++ reviewer-frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '10.234.86.79',
+    host: '10.234.88.55',
     port: 5173,
     },
     resolve: {
