37fa85d89775b1c4199408e082615586e4e47acd
vicorsi <victorhugocorsi@gmail.com>
2024-04-03 15:24:17 -0300

    fix: React app

--- reviewer-frontend/src/main.tsx
+++ reviewer-frontend/src/main.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes'
 
-import { QueryClient, QueryClientProvider} from 'react-query'
-
-const queryClient = new QueryClient()
+import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}/>
-    </QueryClientProvider>
+    <App/>
   </React.StrictMode>
 )
