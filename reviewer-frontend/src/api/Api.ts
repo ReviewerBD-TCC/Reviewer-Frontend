5123005244359fdd75ac97eaac719fac9748b7fe
vicorsi <victorhugocorsi@gmail.com>
2024-04-10 14:59:01 -0300

    fix: Api config

--- reviewer-frontend/src/api/Api.ts
+++ reviewer-frontend/src/api/Api.ts
@@ -8,8 +8,8 @@ export const headers = {
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   };
 
-  export const baseURL =  "http://10.234.84.188:8056/api/v1/"
-  //export const baseURL = "http://10.234.91.91:8056/api/v1"
+  // export const baseURL =  "http://10.234.84.188:8056/api/v1/"
+  export const baseURL = "http://10.234.91.91:8056/api/v1/"
 
 const api = axios.create({ baseURL, headers: headers })
 export default api  
