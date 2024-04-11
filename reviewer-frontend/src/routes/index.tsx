306ff176eb6b874dc00d42afff1efe96c154a4da
byankatomaz <byankatomaz@gmail.com>
2024-04-11 10:31:57 -0300

    fix: import

--- reviewer-frontend/src/routes/index.tsx
+++ reviewer-frontend/src/routes/index.tsx
@@ -2,7 +2,7 @@ import {  RouterProvider, createBrowserRouter } from "react-router-dom";
 import Register from "../pages/Register/Register";
 import Indication from "../pages/Indicacation/Indication";
 import QuestionDb from "../pages/QuestionDb/QuestionDb";
-import CreateForms from "../pages/CreateForms/CreateForms";
+import {CreateForms} from "../pages/CreateForms/CreateForms";
 import Login from "../pages/Login/Login";
 import { Homepage } from "../pages/Homepage/Homepage";
 import EmailIndicationUser from "pages/EmailIndicationUser/EmailIndicationUser";
