8846034006b75c150a7c5864ae8ac388e00bb5e8
vicorsi <victorhugocorsi@gmail.com>
2024-04-12 10:36:02 -0300

    fix: Zod schema validations in register

--- reviewer-frontend/src/validations/ClienteResolver.tsx
+++ reviewer-frontend/src/validations/ClienteResolver.tsx
@@ -2,9 +2,13 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const ClienteValidationSchema = z.object({
-  name: z.string(),
-  email: z.string().email({message: "Insira um email valido"}),
-  password: z.string().min(8),
+  name: z.string().transform(name => {
+    return name.trim().split(' ').map(palavra => {
+      return palavra[0].toLocaleUpperCase().concat(palavra.substring(1))
+    }).join(' ')
+  }),
+  email: z.string().email("Insira um email válido"),
+  password: z.string().min(8, "A senha precisa ter no mínimo 8 caracteres"),
   gkz: z.string(), 
   manager: z.string(),
   user: z.string(),  
