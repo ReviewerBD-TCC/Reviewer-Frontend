b75e8ddca60f6c35bb6c919022b99372a7c325ba
vicorsi <victorhugocorsi@gmail.com>
2024-04-16 14:05:37 -0300

    fix: Form verification

--- reviewer-frontend/src/validations/ClienteResolver.tsx
+++ reviewer-frontend/src/validations/ClienteResolver.tsx
@@ -2,16 +2,16 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const ClienteValidationSchema = z.object({
-  name: z.string().transform(name => {
+  name: z.string().min(1, "Insira um nome válido").transform(name => {
     return name.trim().split(' ').map(palavra => {
       return palavra[0].toLocaleUpperCase().concat(palavra.substring(1))
     }).join(' ')
   }),
   email: z.string().email("Insira um email válido"),
   password: z.string().min(8, "A senha precisa ter no mínimo 8 caracteres"),
-  gkz: z.string(), 
-  manager: z.string(),
-  user: z.string(),  
+  gkz: z.string().min(1, "Insira um departamento válido"), 
+  manager: z.string().min(1, "Insira um nome válido"),
+  user: z.string().min(1, "Insira um usuário válido"),  
   type: z.string(),
 })
 
