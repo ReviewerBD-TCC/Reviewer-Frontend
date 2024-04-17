99385b5a4d8bfed6aabd13fc460e981ba5dc217d
vicorsi <victorhugocorsi@gmail.com>
2024-04-17 16:37:04 -0300

    fix: Register validations

--- reviewer-frontend/src/pages/Register/Register.tsx
+++ reviewer-frontend/src/pages/Register/Register.tsx
@@ -101,7 +101,7 @@ function Register() {
                 },
               })} whenChange={(event) => setValue("user", event.target.value)} placeholder="Nome de usuário" />
 
-              {errors.user && <span className="text-red-600">{errors.user.message}</span>}
+              {errors.user && <span className="text-red-600">Por favor, insira o nome de usuário.</span>}
 
               <SparkTextfield type="text" label="Nome"
                 {...register("name", {
@@ -109,45 +109,45 @@ function Register() {
                     handleSearch("name", value);
                     return value;
                   },
-                })} whenChange={(event) => setValue("name", event.target.value)} placeholder="Nome completo do colaborador" />
+                })} whenChange={(event) => setValue("name", event.target.value)} placeholder="Nome completo" />
 
-              {errors.name && <span className="text-red-600">{errors.name.message}</span>}
+              {errors.name && <span className="text-red-600">Por favor, insira o nome completo.</span>}
               
               <SparkTextfield type="text" label="E-mail"  {...register("email", {
                 setValueAs: (value) => {
                   handleSearch("email", value);
                   return value;
                 },
-              })} whenChange={(event) => setValue("email", event.target.value)} placeholder="E-mail do colaborador" />
+              })} whenChange={(event) => setValue("email", event.target.value)} placeholder="E-mail" />
 
-              {errors.email && <span className="text-red-600">{errors.email.message}</span>}
+              {errors.email && <span className="text-red-600">Por favor, informe um endereço de e-mail válido.</span>}
 
               <SparkTextfield type="text" label="Departamento" {...register("gkz", {
                 setValueAs: (value) => {
                   handleSearch("gkz", value);
                   return value;
                 },
-              })} whenChange={(event) => setValue("gkz", event.target.value)} placeholder="Departamento do colaborador" />
+              })} whenChange={(event) => setValue("gkz", event.target.value)} placeholder="Departamento" />
 
-              {errors.gkz && <span className="text-red-600">{errors.gkz.message}</span>}
+              {errors.gkz && <span className="text-red-600">Por favor, insira o departamento.</span>}
 
               <SparkTextfield type="text" label="Gestor" {...register("manager", {
                 setValueAs: (value) => {
                   handleSearch("manager", value);
                   return value;
                 },
-              })} whenChange={(event) => setValue("manager", event.target.value)} placeholder="Gestor do colaborador" />
+              })} whenChange={(event) => setValue("manager", event.target.value)} placeholder="Gestor" />
 
-              {errors.manager && <span className="text-red-600">{errors.manager.message}</span>}
+              {errors.manager && <span className="text-red-600">Por favor, insira o gestor.</span>}
 
               <SparkTextfield type="password" label="Senha" {...register("password", {
                 setValueAs: (value) => {
                   handleSearch("password", value);
                   return value;
                 },
-              })} whenChange={(event) => setValue("password", event.target.value)} placeholder="Senha do colaborador" />
+              })} whenChange={(event) => setValue("password", event.target.value)} placeholder="Senha" />
 
-              {errors.password && <span className="text-red-600">{errors.password.message}</span>}
+              {errors.password && <span className="text-red-600">A senha precisa ter no mínimo 8 caractéres</span>}
 
               <SparkToggle
                 guid="1"
