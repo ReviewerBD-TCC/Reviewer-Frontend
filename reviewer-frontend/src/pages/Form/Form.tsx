524520379808fccd2eafe83ea0874f986e5eef76
vicorsi <victorhugocorsi@gmail.com>
2024-04-22 16:43:01 -0300

    fix: Form component

--- reviewer-frontend/src/pages/Form/Form.tsx
+++ reviewer-frontend/src/pages/Form/Form.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header, Selected } from 'components'
 import { SparkButton, SparkTextarea } from '@bosch-web-dds/spark-ui-react'
+import { getFormQuestions } from 'services/FormsService'
+import { useAuth } from 'context/AuthProvider'
+import { Form, SendForm } from 'interfaces/SendForm'
 
 function Form() {
 
+    const token = useAuth().accessToken
+    const[dataApi, setDataAPi] = useState<Form[]>([])
+    const [form, setForm] = useState<Form>()
+    const {language} = useAuth();
+   useEffect(()=>{
+    const formQuestion = getFormQuestions(token, 12)
+    const data = formQuestion.then(function(response){
+        const data = response.map(element=>{
+            return element
+        })
+      setDataAPi(data)
+      setForm(data[0])
+    })
+   },[])
+
     const languageOptions = ["Português", "Inglês"]
+
+   console.log(dataApi[0])
 
     return (
         <div className="h-auto min-h-screen w-full flex flex-col items-center">
@@ -13,24 +33,26 @@ function Form() {
                 <div className="w-[85%] h-auto flex flex-col gap-9 pb-7 pt-7">
                     <div className='w-full flex justify-between'>
                         <div className='w-4/5 h-auto'>
-                            <h1 className='font-bold text-4xl'>Feedback os times de automação</h1>
+                            <h1 className='font-bold text-4xl'>{form?.title.charAt(0).toUpperCase() + form?.title.slice(1)}</h1>
                         </div>
                         <div className='w-[20%]'>
                             <Selected labelText='Idioma' zIndex={50} options={languageOptions}/>
                         </div>
                     </div>
                     <div className="">
-                        <p>Este feedback é referente ao ano de 2024</p>
+                        <p>Este feedback é referente ao ano de {form?.year?.split("", 4)}</p>
                     </div>
                     <div>
-                        <div>
-                            <div>
-                                <h2 className='font-bold text-xl' >1 - Titulo da pergunta</h2>
-                            </div>
-                            <div>
-                                <SparkTextarea/>
-                            </div>
+                       { dataApi.map((element, index)=>(
+                        <div className='mt-14 list-decimal'>
+                        {/* <ol className='list-decimal'> */}
+                            <p className='font text-lg' >{index+1} - {language == "Português"?element.questions[index].questionPt:element.questions[index].questionEn}</p>
+                        {/* </ol> */}
+                        <div className='mt-3'>
+                            <SparkTextarea/>
                         </div>
+                    </div>
+                       ))}
                     </div>
                     <div className='flex justify-end'>
                         <SparkButton text='Enviar' onClick={()=>{}}/>
