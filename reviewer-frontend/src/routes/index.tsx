f45c700c46bb527a06d73643e9dcc71476df68e1
byankatomaz <byankatomaz@gmail.com>
2024-04-29 16:51:51 -0300

    add: private route

--- reviewer-frontend/src/routes/index.tsx
+++ reviewer-frontend/src/routes/index.tsx
@@ -2,50 +2,76 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Register from "../pages/Register/Register";
 import Indication from "../pages/Indicacation/Indication";
 import QuestionDb from "../pages/QuestionDb/QuestionDb";
-import {CreateForms} from "../pages/CreateForms/CreateForms";
-import Login from "../pages/Login/Login";
+import { CreateForms } from "../pages/CreateForms/CreateForms";
+import {Login} from "../pages/Login/Login";
 import { Homepage } from "../pages/Homepage/Homepage";
 import EmailIndicationUser from "pages/EmailIndicationUser/EmailIndicationUser";
 import Form from "pages/Form/Form";
+import { PrivateRoute } from "components";
 
 
 export const route = createBrowserRouter([
-    {
-      path: "/",
-      element: <Login/>
-    },
-    {
-      path: "/register",
-      element: <Register/>,
-    },
-    {
-      path: "/indication",
-      element: <Indication/>,
-    },
-    {
-      path: "/home",
-      element: <Homepage/>,
-    },
-    {
-      path: "/questions",
-      element: <QuestionDb/>
-    },
-    {
-      path: "/createform",
-      element: <CreateForms/>
-    },
-    {
-      path: "/send-indication",
-      element: <EmailIndicationUser/>
-    },
-    {
-      path: "/form",
-      element: <Form/>
-    }
-  ]);
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
 
-  export default function AppRoutes() {
-    return (
-          <RouterProvider router={route} />
-    );
+    path: "/indication",
+    element: (
+      <PrivateRoute>
+        <Indication />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/home",
+    element: (
+      <PrivateRoute>
+        <Homepage />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/questions",
+    element: (
+      <PrivateRoute>
+        <QuestionDb />
+      </PrivateRoute>
+    )
+  },
+  {
+    path: "/createform",
+    element: (
+      <PrivateRoute>
+        <CreateForms />
+      </PrivateRoute>
+    )
+  },
+  {
+    path: "/send-indication",
+    element: (
+      <PrivateRoute>
+        <EmailIndicationUser />
+      </PrivateRoute>
+    )
+  },
+  {
+    path: "/form",
+    element: (
+      <PrivateRoute>
+        <Form />
+      </PrivateRoute>
+    )
   }
+]);
+
+export default function AppRoutes() {
+  return (
+    <RouterProvider router={route} />
+  );
+}
