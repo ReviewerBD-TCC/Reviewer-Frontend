ea635679f48b51fafdc9b53a51f05f60d47a1e1c
vicorsi <victorhugocorsi@gmail.com>
2024-04-29 09:54:07 -0300

    fix: Added search in table

--- reviewer-frontend/src/components/Table/Table.tsx
+++ reviewer-frontend/src/components/Table/Table.tsx
@@ -3,6 +3,8 @@ import { UserService } from "services/UserService";
 import { useAuth } from "context/AuthProvider";
 import { mailSender } from "services/EmailServices";
 import { Email } from "interfaces/Emaill";
+import { useQuery } from "react-query";
+import { SparkSearchBar, SparkTextfield } from "@bosch-web-dds/spark-ui-react";
 
 export interface TableUser{
     guid: number
@@ -16,17 +18,17 @@ export const TableUser:React.FC = () =>{
 
     useEffect(()=>{
         dataUser.then(function(response){
-            const user = response.map((user)=>{
+            const user = response.map((user:any)=>{
                return user
             })
             setUsers(user)
         })
     }, [])
 
-
-
     const [users, setUsers] = useState<string[]>([])
     const {selectedUsers, selectUser} = useAuth()
+    const [search, setSearch] = useState('')
+
     const checkedUsers = (value: any, checked:boolean)=>{
         if (checked == true){
             selectUser(value)
@@ -38,8 +40,16 @@ export const TableUser:React.FC = () =>{
             })
         }
     };
-    
+
+    const filteredUsers = users.filter((user) => user.name.includes(search));
+
+    console.log(search)
+
     return(
+        <div className="">
+        <div className="flex w-full max-w-full">
+            <SparkTextfield type="search" placeholder="Digite o nome do colaborador." value={search} onClick={(e)=>setSearch(e.target.value)} />
+        </div>
         <div id="tableUser" className="w-auto h-40 max-h-62 bg-boschWhite overflow-y-auto p-1">
             <table className="w-full justify-start flex flex-col border-collapse">
                 <thead className="w-full bg-boschWhite h-9 flex items-center p-3 border-b-[1.5px] border-boschBlack">
@@ -47,7 +57,7 @@ export const TableUser:React.FC = () =>{
                 </thead>
                 <tbody className="w-full h-auto bg-red-200 overflow-y-auto">
                 {
-                    users?.map((user:any) => (
+                    filteredUsers?.map((user:any) => (
                     <tr className="border-b border-slate-300 w-full h-12 flex flex-row items-center justify-between bg-boschWhite p-3 hover:bg-boschLightGray">
                         <td className="w-[4%] h-full flex justify-center items-center"><input type="checkbox" className="w-4 h-4 bg-gray-200 " value={user.email} name="checkbox" id="inputId" onClick={(e)=>{
                             
@@ -62,5 +72,7 @@ export const TableUser:React.FC = () =>{
                 </tbody>
             </table>
         </div>
+        </div>
+
     )
 }
