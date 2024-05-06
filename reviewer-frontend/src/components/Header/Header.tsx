9cc0bb34c32221c603eb259286f941b4d4bd7950
jzsantana <juliass7187@gmail.com>
2024-05-06 16:26:32 -0300

    fix: created a function to get the logged-in user

--- reviewer-frontend/src/components/Header/Header.tsx
+++ reviewer-frontend/src/components/Header/Header.tsx
@@ -1,17 +1,30 @@
+import { useAuth } from 'context/AuthProvider'
 import Logo from '../../assets/images/Logo.png'
 import Supergraphic from '../../assets/images/Supergraphic.png'
 import { Link } from 'react-router-dom'
+import { SparkIcon } from '@bosch-web-dds/spark-ui-react' 
 
 export function Header() {
+
+  const { user } = useAuth();
+
   return (
     <div className='border-b w-full bg-white top-0 flex justify-center items-center flex-col relative'>
         <img src={Supergraphic} alt="Supergraphic" />
         
-        <div className='w-[80%] p-1'>
-          <Link to={'/home'}>
+        <div className='w-[80%] p-1 flex flex-row justify-between items-center'>
+          <Link className="w-[75%]" to={'/home'}>
             <img src={Logo} alt="CompanyLogo" className='w-44'/>
           </Link>
+
+          <p className='font-medium text-[#808080] text-[14px]'>{user?.name}</p>
+          <div className='w-auto'>
+            <SparkIcon icName='logout' pallete='secondary' />
+          </div>
+          
         </div>
+
+
     </div>
   )
 }
