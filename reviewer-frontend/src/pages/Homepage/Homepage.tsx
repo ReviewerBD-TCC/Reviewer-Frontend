64e69881bbf3fded753ae359eea5a807a920b544
jzsantana <juliass7187@gmail.com>
2024-05-07 08:09:52 -0300

    fix: color variables and style for the cards

--- reviewer-frontend/src/pages/Homepage/Homepage.tsx
+++ reviewer-frontend/src/pages/Homepage/Homepage.tsx
@@ -5,12 +5,6 @@ export const Homepage = () => {
 
     const [cardList]: any = useState([
         {
-            key:1,
-            title: <h2 className='text-white font-bold text-lg'>Visualizar <br></br>Formulário</h2>,
-            backgroundColor: 'bg-boschRed',
-            nav: '/form'
-        },
-        {
             key:2,
             title: <h2 className='text-white font-bold text-lg'>Banco de <br></br>perguntas</h2>,
             backgroundColor: 'bg-boschPurple',
@@ -30,21 +24,27 @@ export const Homepage = () => {
         },
         {
             key:5,
-            title: <h2 className='text-white font-bold text-lg'>Visualizar Dashboard<br></br></h2>,
+            title: <h2 className='text-white font-bold text-lg'>Visualizar <br /> Dashboard</h2>,
             backgroundColor: 'bg-boschGreen',
             nav: '/dashboard'
-        }
+        },
+        {
+            key:1,
+            title: <h2 className='text-white font-bold text-lg'>Visualizar <br></br>Formulário</h2>,
+            backgroundColor: 'bg-boschRed',
+            nav: '/form'
+        },
     ])
 
   return (
     <div className={`bg-[#fff] w-full h-screen flex justify-center items-center flex-col`}>
         <Header/>
-            <div className="bg-boschWhite h-screen w-[90%] flex items-center justify-center flex-col">
-                <div className='bg-[#fff] w-[85%] h-full flex flex-col justify-center items-center'>
+            <div className="bg-boschWhite h-screen w-full flex items-center justify-center flex-col">
+                <div className='bg-[#fff] w-[78%] p-4 h-full flex flex-col justify-center items-center'>
                     <div className='w-full h-16 flex'>
                         <h1 className='font-bold text-3xl'>Painel administrador</h1>
                     </div>
-                    <div className='w-full h-2/4 flex justify-between items-center flex-row gap-2'>
+                    <div className='w-full h-2/4 flex justify-around items-center flex-row gap-2 bg-boschWhite'>
                         {cardList.map((i:any) => (
                             <Card key={i.key} text={i.title} backgroundColor={i.backgroundColor} nav={i.nav}/>
                         ))}
