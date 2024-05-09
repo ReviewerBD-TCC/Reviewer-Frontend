5e10dc257047e419369943fd9dd363c9b13b557e
byankatomaz <byankatomaz@gmail.com>
2024-05-09 08:59:02 -0300

    fix: create the interface for QuestionForm

--- reviewer-frontend/src/interfaces/SendForm.ts
+++ reviewer-frontend/src/interfaces/SendForm.ts
@@ -5,6 +5,11 @@ export interface Answer{
     answer: string
 }
 
+export interface QuestionAnswer{
+    question: number,
+    answer: Answer
+}
+
 export interface Form{
     questionFormId: number,
     userId: number,
