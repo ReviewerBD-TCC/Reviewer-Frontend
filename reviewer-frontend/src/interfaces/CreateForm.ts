40b5d7f2d45248ce1eb6765548233c6d0ca6baea
byankatomaz <byankatomaz@gmail.com>
2024-05-13 16:20:32 -0300

    feat: update CreateForm.ts

--- reviewer-frontend/src/interfaces/CreateForm.ts
+++ reviewer-frontend/src/interfaces/CreateForm.ts
@@ -1,7 +1,16 @@
+import { QuestionProps } from "./Question";
 import { QuestionList } from "./QuestionList";
 
 export interface FormInterface{
+    id: number;
     title: string;
     year: Date;
     questions?: QuestionList[];
+}
+
+export interface FormResponseInterface{
+    id: number;
+    title: string;
+    year: Date;
+    questions?: QuestionProps[];
 }
