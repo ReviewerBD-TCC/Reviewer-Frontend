533eb5b18a129052286f2e79acce82153aa76e7c
hora-keven <kevensantos993@gmail.com>
2024-05-14 12:49:00 -0300

    feat: card form icon delete

--- reviewer-frontend/src/components/CardForm/CardForm.tsx
+++ reviewer-frontend/src/components/CardForm/CardForm.tsx
@@ -1,21 +1,29 @@
+import { SparkButton, SparkIcon } from '@bosch-web-dds/spark-ui-react';
 import { AllFormsInterface } from 'interfaces/AllForms'
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-interface cardFormProps{
+interface cardFormProps {
   titleForm: AllFormsInterface["title"];
   id: number;
 }
 
-const CardForm: React.FC<cardFormProps> = ({titleForm, id}: cardFormProps) => {
+const CardForm: React.FC<cardFormProps> = ({ titleForm, id }: cardFormProps) => {
   return (
-    <Link to={`/all-forms/single-form/${id}`}>
-      <div className='bg-boschLightGray w-full min-h-[3.8rem] p-4'> 
+    
+      <div className='bg-boschLightGray w-full min-h-[3.8rem] p-4 flex-row flex items-center'>
+        <Link to={`/all-forms/single-form/${id}`} className='w-[90%]'>
         <h2 className='font-bold text-xl'>
-            {titleForm}
+          {titleForm}
         </h2>
+        </Link>
+        <div className='flex justify-end items-end w-[10%]'>
+          <button className='w-auto bg-boschGrayText/10 rounded-full' onClick={()=>console.log(id)}>
+            <SparkIcon icName={"delete"} />
+          </button>
+        </div>
       </div>
-    </Link>
+    
   )
 }
 
