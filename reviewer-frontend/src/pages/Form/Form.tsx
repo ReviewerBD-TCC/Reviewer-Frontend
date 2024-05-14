1ed9ef3c1d6768750df8983a39906c41b74c2c54
vicorsi <victorhugocorsi@gmail.com>
2024-05-14 16:31:54 -0300

    fix: Log text

--- reviewer-frontend/src/pages/Form/Form.tsx
+++ reviewer-frontend/src/pages/Form/Form.tsx
@@ -56,7 +56,7 @@ const FormComponent = () => {
 
         setFormData(currentFormFiltered || null);
       } catch (error) {
-        console.error("Erro ao carregar formulários:", error);
+        console.error("Erro ao carregar formulários: ", error);
       }
     };
 
