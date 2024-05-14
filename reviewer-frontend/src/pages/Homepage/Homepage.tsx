d78f697a453ff62a943e6ab1570de2dec18a5079
byankatomaz <byankatomaz@gmail.com>
2024-05-14 12:29:16 -0300

    fix: update Homepage.tsx

--- reviewer-frontend/src/pages/Homepage/Homepage.tsx
+++ reviewer-frontend/src/pages/Homepage/Homepage.tsx
@@ -6,33 +6,38 @@ export const Homepage = () => {
     const [cardList]: any = useState([
         {
             key:2,
-            title: <h2 className='text-white font-bold text-lg'>Banco de <br></br>perguntas</h2>,
+            title: <h2 className='text-white font-bold xl:text-xl lg:text-lg'>Banco de <br></br>perguntas</h2>,
             backgroundColor: 'bg-boschPurple',
-            nav: '/questions'
+            nav: '/questions',
+            className: ''
         },
         {
             key:3,
-            title: <h2 className='text-white font-bold text-lg'>Criação de <br></br>Feedback</h2>,
+            title: <h2 className='text-white font-bold xl:text-xl lg:text-lg'>Criação de <br></br>Formulário</h2>,
             backgroundColor: `bg-boschBlue`,
-            nav: '/createform'
+            nav: '/create-form',
+            className: ''
         },
         {
             key:4,
-            title: <h2 className='text-white font-bold  text-lg'>Enviar <br></br>Formulário</h2>,
+            title: <h2 className='text-white font-bold xl:text-xl lg:text-lg'>Enviar <br></br>Formulário de Indicação</h2>,
             backgroundColor: 'bg-boschTurquoise',
-            nav: '/send-indication'
+            nav: '/send-indication',
+            className: ''
         },
         {
             key:5,
-            title: <h2 className='text-white font-bold text-lg'>Visualizar <br /> Dashboard</h2>,
+            title: <h2 className='text-white font-bold xl:text-xl lg:text-lg'>Visualizar <br /> Dashboard</h2>,
             backgroundColor: 'bg-boschGreen',
-            nav: '/dashboard'
+            nav: '/dashboard',
+            className: ''
         },
         {
             key:1,
-            title: <h2 className='text-white font-bold text-lg'>Visualizar <br></br>Formulário</h2>,
+            title: <h2 className='text-white font-bold xl:text-xl lg:text-lg'>Visualizar <br></br>Formulários</h2>,
             backgroundColor: 'bg-boschRed',
-            nav: '/all-forms'
+            nav: '/all-forms',
+            className: ''
         },
     ])
 
@@ -40,13 +45,13 @@ export const Homepage = () => {
     <div className={`bg-[#fff] w-full h-screen flex justify-center items-center flex-col`}>
         <Header/>
             <div className="bg-boschWhite h-screen w-full flex items-center justify-center flex-col">
-                <div className='bg-[#fff] w-[78%] p-4 h-full flex flex-col justify-center items-center'>
+                <div className='bg-[#fff] w-[90%] p-4 h-full flex flex-col justify-center items-center'>
                     <div className='w-full h-16 flex'>
                         <h1 className='font-bold text-3xl'>Painel administrador</h1>
                     </div>
-                    <div className='w-full h-2/4 flex justify-around items-center flex-row gap-2 bg-boschWhite'>
+                    <div className='w-full h-2/4 flex justify-around items-center flex-row gap-4 bg-boschWhite'>
                         {cardList.map((i:any) => (
-                            <Card key={i.key} text={i.title} backgroundColor={i.backgroundColor} nav={i.nav}/>
+                            <Card key={i.key} text={i.title} backgroundColor={i.backgroundColor} nav={i.nav} className={"hover:h-96"}/>
                         ))}
                     </div>
                 </div>
