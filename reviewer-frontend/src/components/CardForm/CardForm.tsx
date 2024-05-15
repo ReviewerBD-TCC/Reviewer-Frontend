50a3dd1eb6967549d9e8c069ba29f18fbeb4f28b
vicorsi <victorhugocorsi@gmail.com>
2024-05-15 14:06:51 -0300

    feat: Delete form

--- reviewer-frontend/src/components/CardForm/CardForm.tsx
+++ reviewer-frontend/src/components/CardForm/CardForm.tsx
@@ -1,7 +1,11 @@
-import { SparkButton, SparkIcon } from '@bosch-web-dds/spark-ui-react';
+import { SparkIcon } from '@bosch-web-dds/spark-ui-react';
 import { AllFormsInterface } from 'interfaces/AllForms'
+import { useAuth } from 'context/AuthProvider';
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { ToastContainer, Bounce, toast } from "react-toastify";
+import ModalConfirmation from 'components/Modal/ModalConfirmation';
+import useModal from "../../hooks/useModal";
 
 interface cardFormProps {
   titleForm: AllFormsInterface["title"];
@@ -10,6 +14,27 @@ interface cardFormProps {
 }
 
 const CardForm: React.FC<cardFormProps> = ({ titleForm, id, className }: cardFormProps) => {
+
+  const { accessToken } = useAuth();
+  
+  const {isOpen, toggle} = useModal()
+
+  // async function deleteForm(token: string | null, formId: number | null) {
+  //   try {
+  //       const response = await FormService.deleteForm(token, formId);
+  //       if(response.status === 204){
+  //         showToastMessage()
+  //         setTimeout(
+  //           ()=>{
+  //             window.location.reload()
+  //           }
+  //         )
+  //       }
+  //   } catch (error) {
+  //       console.error(error);
+  //   }
+  // }
+
   return (
     
       <div className={`bg-boschLightGray w-full min-h-[3.8rem] p-4 flex-row flex items-center  ${className}`}>
@@ -19,10 +44,20 @@ const CardForm: React.FC<cardFormProps> = ({ titleForm, id, className }: cardFor
           </h2>
         </Link>
         <div className='flex justify-end items-end w-[10%]'>
-          <button className='w-auto bg-boschGrayText/10 rounded-full' onClick={()=>console.log(id)}>
+          <button className='w-auto bg-boschGrayText/10 rounded-full' onClick={toggle}>
             <SparkIcon icName={"delete"} />
           </button>
+          <ModalConfirmation
+            isOpen={isOpen} 
+            toggle={toggle} 
+            modalButtonText="Sim" 
+            modalSecondButtonText="Não" 
+            modalTitle="Deletar formulário" 
+            modalSubtitle="Ao confirmar, o formulários será deletado permanentemente, assim como suas respostas." 
+            formId={id} 
+          />
         </div>
+        <ToastContainer/>
       </div>
     
   )
