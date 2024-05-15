3420ed83b9f7e0f0fa7853d5780cef20bcaa1ca6
jzsantana <juliass7187@gmail.com>
2024-05-15 14:46:03 -0300

    fix: style for select user modal

--- reviewer-frontend/src/components/Modal/ModalEmailConfirmation.tsx
+++ reviewer-frontend/src/components/Modal/ModalEmailConfirmation.tsx
@@ -1,24 +1,18 @@
 import { SparkButton, SparkTextarea, SparkTextfield, SparkToggle } from "@bosch-web-dds/spark-ui-react";
-import { AxiosResponse } from "axios";
-import React, { ReactNode } from "react"
-import api from "../../api/Api";
-
-import { zodResolver } from '@hookform/resolvers/zod';
-import  z  from 'zod' 
-
+import { ToastContainer, Bounce, toast } from "react-toastify";
+import { EmailResolver } from "validations/EmailResolver";
 import { SubmitHandler, useForm } from 'react-hook-form'
-
+import { zodResolver } from '@hookform/resolvers/zod';
+import { mailSender } from "services/EmailServices";
+import ModalEmailSelect from "./ModalEmailSelect";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "context/AuthProvider";
 import useModal from "../../hooks/useModal";
-
-import ModalEmailSelect from "./ModalEmailSelect";
 import { Email } from "interfaces/Emaill";
-import { EmailResolver } from "validations/EmailResolver";
-import { mailSender } from "services/EmailServices";
-
-import { ToastContainer, Bounce, toast } from "react-toastify";
-
-import { useNavigate } from "react-router-dom";
+import React, { ReactNode } from "react"
+import { AxiosResponse } from "axios";
+import api from "../../api/Api";
+import  z  from 'zod' 
 
 interface ModalProps{
     data: Email
@@ -27,12 +21,9 @@ interface ModalProps{
 }
 
 const ModalEmailConfirmation:React.FC<ModalProps> = (props) => {
-
     const navigate = useNavigate()
-
     const { accessToken } = useAuth();
     const token = accessToken;
-
     const {isOpen, toggle} = useModal()
 
     const sendEmail: SubmitHandler<Email> = async (values) => {
@@ -62,8 +53,8 @@ const ModalEmailConfirmation:React.FC<ModalProps> = (props) => {
         <>{props.isOpen && (
             <div className="fixed inset-0 z-50 flex items-center justify-center" onClick={props.toggle}>
                 <div className="flex justify-center items-center bg-black bg-opacity-40 w-full h-full ">
-                    <div className="flex bg-boschWhite w-[45%] 2xl:h-[40%] md:h-[40%]" onClick={(e)=>e.stopPropagation()}>
-                        <div className="w-[80%] h-auto flex flex-col justify-center gap-10 m-auto">
+                    <div className="flex bg-boschWhite w-[40rem] max-w-[40rem] h-auto pt-6 pb-6 pr-8 pl-8" onClick={(e)=>e.stopPropagation()}>
+                        <div className="w-full h-auto flex flex-col justify-center gap-10 m-auto">
                             <div className="flex flex-col gap-2">
                                 <h1 className="text-3xl font-bold">Indicação pronta</h1>
                                 <p>Selecione os colaboradores a receberem o formulário.</p>
