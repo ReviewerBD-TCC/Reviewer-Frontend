ae7ee6bf8f279de83cb39770b5cc35b237a30e0d
byankatomaz <byankatomaz@gmail.com>
2024-05-17 11:27:19 -0300

    update FormService.ts

--- reviewer-frontend/src/services/FormService.ts
+++ reviewer-frontend/src/services/FormService.ts
@@ -1,10 +1,9 @@
-import api, { headers } from "../api/Api";
-import { CreateFormInterface } from "interfaces/CreateForm";
+import api from "../api/Api";
 import { FormInterface } from "interfaces/CreateForm";
-import { updateQuestion } from "interfaces/SendForm";
+import { UpdateQuestion } from "interfaces/SendForm";
 
 
-const createForm = async (data: CreateFormInterface, token: string ) => {
+const createForm = async (data: FormInterface, token: string) => {
     try {
         const response = await api.post('/form', data, {
             headers: {
@@ -15,68 +14,69 @@ const createForm = async (data: CreateFormInterface, token: string ) => {
 
         return response;
     } catch (error) {
-        throw error;
+        console.error(error);
     }
 }
 
 const getFormQuestions = async (token: string | null, formId: number | null) => {
     try {
-      const response = await api.get(`/form/${formId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-        ,
-      });
-      return response.data;
+        const response = await api.get(`/form/${formId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+            ,
+        });
+        return response.data;
     } catch (error) {
-      throw error;
+        console.error(error);
     }
 }
 
 const getAllForms = async (token: string | null) => {
-  try{
-      const response = await api.get(`/form`, {
-          headers:{
-              Authorization: `Bearer ${token}`
-          },
-      }
-      )
-      return response.data
-  }
-  catch(error){
-      throw(error)
-  }
+    try {
+        const response = await api.get(`/form`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+        }
+        )
+        return response.data
+    }
+    catch (error) {
+        console.error(error);
+    }
 }
 
-const deleteForm = async (token: string | null, formId: number | any, data: updateQuestion) => {
+const deleteForm = async (token: string | null, formId: number) => {
     try {
         const response = await api.delete(`/form/${formId}`,
-         {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
         return response;
     } catch (error) {
-        throw(error);
+        console.error(error);
     }
 }
 
-const editFormQuestion = async (token: string | null, formId: number | any, data: updateQuestion) => {
-    try{
+const editFormQuestion = async (token: string | null, formId: number, data: UpdateQuestion) => {
+    try {
         const response = await api.patch(`/form/${formId}`,
-        data,
-        {
-            headers: {
-                Authorization: `Bearer ${token}`
+            data,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
             }
-        }
         );
         return response;
-    } catch(error){
-        throw(error);
+    } catch (error) {
+        console.error(error);
     }
-}
+};
+
 
 
 
@@ -87,4 +87,4 @@ export const FormService = {
     getAllForms,
     deleteForm,
     editFormQuestion
-};
+}
