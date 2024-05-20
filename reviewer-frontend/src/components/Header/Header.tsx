c964a01e8e5de3a00e6e55309320292d37cf5dce
jzsantana <juliass7187@gmail.com>
2024-05-20 16:02:29 -0300

    fix: header style

--- reviewer-frontend/src/components/Header/Header.tsx
+++ reviewer-frontend/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from "context/AuthProvider";
-import Logo from "../../assets/images/Logo.png";
+import Logo from "../../assets/images/logo_symbol_red_black.png";
 import Supergraphic from "../../assets/images/Supergraphic.png";
 import { Link } from "react-router-dom";
 import { SparkIcon } from "@bosch-web-dds/spark-ui-react";
@@ -14,29 +14,32 @@ export function Header() {
 
   return (
     <div className="border-b w-full bg-white top-0 flex justify-center items-center flex-col relative">
-      <img src={Supergraphic} alt="Supergraphic" />
+      <img src={Supergraphic} alt="Supergraphic" className="w-full" />
 
-      <div className="w-[90%] p-1 flex flex-row justify-between items-center bg-boschWhite">
-        <Link className="w-[75%]" to={"/home"}>
-          <img src={Logo} alt="CompanyLogo" className="w-44" />
+      <div className="w-[90%] flex flex-row justify-between items-center bg-boschWhite">
+        <Link className="w-[70%] pt-4 pb-4" to={"/home"}>
+          <img src={Logo} alt="CompanyLogo" className="w-40" />
         </Link>
-        <div className="w-auto">
-          <p className="font-medium text-boschGrayText text-[14px]">
-            {user?.name}
-          </p>
-        </div>
-        {location.pathname !== "/register" && (
+
+        <div className="w-[25%] flex flex-row items-center gap-6">
+          <div className="w-[90%] text-end">
+            <p className="font-medium text-boschGrayText text-[15px]">
+              {user?.name}
+            </p>
+          </div>
+
+          {location.pathname !== "/register" && (
           <div className="w-auto">
-            <button className="w-auto h-auto">
+            <button className="w-auto h-auto flex justify-center items-center">
               <SparkIcon
                 icName="logout"
-                pallete="secondary"
                 noPadding={true}
                 onClick={() => logout()}
-              />
-            </button>
-          </div>
-        )}
+                />
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
