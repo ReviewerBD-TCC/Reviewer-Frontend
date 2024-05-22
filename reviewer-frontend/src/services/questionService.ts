a12d84fbb523788f12c28e037fe0cfd1dd8e90f8
byankatomaz <byankatomaz@gmail.com>
2024-05-22 08:04:46 -0300

    update questionService.ts

--- reviewer-frontend/src/services/questionService.ts
+++ reviewer-frontend/src/services/questionService.ts
@@ -1,4 +1,6 @@
+import { ModalProps } from "interfaces/ModalInterfaces/Modal";
 import api from "../api/Api";
+import { QuestionProps } from "interfaces/QuestionsInterface/Question";
 
 const getQuestions = async (token: string | null) => {
   try {
@@ -13,7 +15,35 @@ const getQuestions = async (token: string | null) => {
   }
 }
 
-const updateQuestion = async (token: string | null, id: number, active: boolean) => {
+const postQuestion = async (token: string | null, data: ModalProps) => {
+  try {
+    const response = await api.post('question', data, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+
+}
+
+const updateQuestion = async (token: string | null, data: QuestionProps, id: number) => {
+  try {
+    const response = await api.put(`question/${id}`, data, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    return response
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+
+const updateQuestionActive = async (token: string | null, id: number, active: boolean) => {
 
   try {
     const response = await api.patch(
@@ -33,6 +63,11 @@ const updateQuestion = async (token: string | null, id: number, active: boolean)
   }
 }
 
+
+
 export const QuestionService = {
-  getQuestions, updateQuestion
+  getQuestions,
+  updateQuestion,
+  updateQuestionActive,
+  postQuestion
 };
