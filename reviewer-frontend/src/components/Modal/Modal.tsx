1fa520ec74b59a7cabee445a4027a97867c649d0
byankatomaz <byankatomaz@gmail.com>
2024-05-22 07:56:26 -0300

    update Modal.tsx

--- reviewer-frontend/src/components/Modal/Modal.tsx
+++ reviewer-frontend/src/components/Modal/Modal.tsx
@@ -1,134 +1,161 @@
-import { SparkButton, SparkTextarea, SparkTextfield, SparkToggle } from "@bosch-web-dds/spark-ui-react";
-import React, { ReactNode, useContext, useState } from "react"
+import {
+  SparkButton,
+  SparkTextfield,
+  SparkToggle,
+} from "@bosch-web-dds/spark-ui-react";
+import React, { useState } from "react";
 import { ToastContainer, Bounce, toast } from "react-toastify";
-import { useAuth, useQuestion } from "context/AuthProvider";
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form'
-import { AxiosResponse } from "axios";
-import api from "../../api/Api";
-import  z  from 'zod' 
+import { useAuth } from "context/AuthProvider";
+import { useForm } from "react-hook-form";
+import { ModalProps } from "interfaces/ModalInterfaces/Modal";
+import { QuestionService } from "services/QuestionService";
+import { CreateQuestionResolver } from "validations/CreateQuestionResolver";
 
-const schema = z.object({
-    titlePtValue: z.string(),
-    titleEnValue: z.string(),
-    active: z.boolean().default(true)
+const Modal: React.FC<ModalProps> = (props: ModalProps) => {
+  const id = props.id;
+  const { accessToken } = useAuth();
+  const [active, setActive] = useState<boolean>(true);
+
+  const showToastMessage = () => {
+    toast.success("Pergunta foi editada com sucesso!", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+  };
+
+  const showToastMessageError400 = () => {
+    toast.warning("Por favor preencha os campos corretamente!", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+  };
+
+  const { handleSubmit, register } = useForm<ModalProps>({
+    defaultValues: {
+      questionPt: props.questionPt,
+      questionEn: props.questionEn,
+      active: active,
+    },
+    mode: "onChange",
+    reValidateMode: "onChange",
+    resolver: CreateQuestionResolver,
   });
 
-interface ModalProps{
-    id: number,
-    titlePtValue: string,
-    titleEnValue: string,
-    active: boolean,
-    title: string,
-    children?: ReactNode;
-    isOpen: boolean;
-    toggle: () => void
-}
+  const submitQuestion = async (props: ModalProps) => {
+    try {
+      const question: ModalProps = {
+        questionPt: props.questionPt,
+        questionEn: props.questionEn,
+        active: active,
+      };
 
-const Modal:React.FC<ModalProps> = (props) => {
-    const id = props.id
-    const { accessToken,  setActiveValue } = useAuth();
-    const [active, setActive] = useState<boolean>(props.active);
-    const token = accessToken
+      if (id) {
+        const { status } = await QuestionService.updateQuestion(
+          accessToken,
+          question,
+          id
+        );
 
-    // const dropdownOptions =
-    //  '[{"label":"Dissertativa","value":"2"}]'
-
-    const showToastMessage = () => {
-        toast.success('Pergunta foi editada com sucesso!', {
-          position: "top-right",
-          autoClose: 1500,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-          transition: Bounce,
-        });
-      }
-
-    const showToastMessageError400 = () => {
-        toast.warning('Por favor preencha os campos corretamente!', {
-          position: "top-right",
-          autoClose: 1500,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-          transition: Bounce,
-        });
-      }
-
-    const {
-        handleSubmit,
-        register,
-        formState: { defaultValues },
-      } = useForm<ModalProps>({
-        defaultValues: { titlePtValue: props.titlePtValue, titleEnValue: props.titleEnValue, active: active },
-        mode: 'onChange',
-        reValidateMode: 'onChange',
-        resolver: zodResolver(schema),
-      });
-    
-    async function updateQuestion(props :ModalProps) {
-        // console.log("setouuu", active)
-        try{
-            const response: AxiosResponse = await api.put(
-                `question/${id}`,
-                {
-                    questionPt: props.titlePtValue,
-                    questionEn: props.titleEnValue,
-                    active: active,
-                },{
-                    headers: {
-                        'Authorization' : `Bearer ${token}`
-                    }
-                }
-            );
-            console.log(response.data.question);
-            showToastMessage()
-            setTimeout(() => {
-            console.log(accessToken)
-            window.location.reload()
-            }, 1000)
-        }catch(error){
-            if(error.response.status === 400){
-                showToastMessageError400()
-            }
+        if (status === 202) {
+          showToastMessage();
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
         }
+      } else {
+        const { status } = await QuestionService.postQuestion(
+          accessToken,
+          question
+        );
+
+        if (status === 201) {
+          showToastMessage();
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+        }
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    return (
-        <>{props.isOpen && (
-            <div className="fixed inset-0 z-50 flex items-center justify-center" onClick={props.toggle}>
-                <div className="flex justify-center items-center bg-black bg-opacity-40 w-full h-full ">
-                    <div className="flex bg-boschWhite max-w-[40rem] w-[40rem] pt-6 pb-6 pl-8 pr-8" onClick={(e)=>e.stopPropagation()}>
-                        {props.children}
-                        <div className="w-full h-auto flex flex-col justify-center gap-8 m-auto">
-                            <h1 className="text-3xl font-bold">{props.title}</h1>
-                            <div className="flex justify-end items-end"> 
-                                <SparkToggle onClick={()=>{}} whenChange={()=>{setActive(!active)}} leftLabel="Pergunta ativa" guid="spark-toggle-right-label" selected={active}/>
-                            </div>
-                            <div className="flex flex-col gap-4">
-                                <SparkTextfield {...register('titlePtValue')} label="Português" defaultValue={props.titlePtValue} placeholder="Digite a pergunta em português"/>
-                                <SparkTextfield {...register('titleEnValue')}label="Inglês" value={props.titleEnValue} placeholder="Digite a pergunta em inglês"/>
-                            </div>
-                            <div className="flex items-end justify-end gap-4">
-                                <SparkButton text="Cancelar" pallete="secondary" onClick={(props.toggle)} ></SparkButton>
-                                <SparkButton text="Salvar" onClick={handleSubmit(updateQuestion)}/>
-                            </div> 
-                        </div>
-                    </div>
+  return (
+    <>
+      {props.isOpen && (
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center"
+          onClick={props.toggle}
+        >
+          <div className="flex justify-center items-center bg-black bg-opacity-40 w-full h-full">
+            <div
+              className="flex bg-boschWhite max-w-[40rem] w-[40rem] pt-6 pb-6 pl-8 pr-8"
+              onClick={(e) => e.stopPropagation()}
+            >
+              {props.children}
+              <div className="w-full h-auto flex flex-col justify-center gap-8 m-auto">
+                <h1 className="text-3xl font-bold">{props.title}</h1>
+                {id && (
+                  <div className="flex justify-end items-end">
+                    <SparkToggle
+                      onClick={() => {}}
+                      whenChange={() => {
+                        setActive(!active);
+                      }}
+                      leftLabel="Pergunta ativa"
+                      guid="spark-toggle-right-label"
+                      selected={active}
+                    />
+                  </div>
+                )}
+                <div className="flex flex-col gap-4">
+                  <SparkTextfield
+                    {...register("questionPt")}
+                    label="Português"
+                    defaultValue={props.questionPt}
+                    placeholder="Digite a pergunta em português"
+                  />
+                  <SparkTextfield
+                    {...register("questionEn")}
+                    label="Inglês"
+                    defaultValue={props.questionEn}
+                    placeholder="Digite a pergunta em inglês"
+                  />
                 </div>
-                <ToastContainer/>
-            </div>
-            )}
-        </>
-    );
-  }
-  
+                <div className="flex items-end justify-end gap-4">
+                  <SparkButton
+                    text="Cancelar"
+                    pallete="secondary"
+                    onClick={props.toggle}
+                  />
 
-export default Modal
+                  <SparkButton
+                    text="Salvar"
+                    onClick={handleSubmit(submitQuestion)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <ToastContainer />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
