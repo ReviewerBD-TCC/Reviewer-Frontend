a62f14163304551b02ab213215677bf257342b83
byankatomaz <byankatomaz@gmail.com>
2024-05-22 07:55:41 -0300

    update reviewer-frontend/src/components/index.ts

--- reviewer-frontend/src/components/index.ts
+++ reviewer-frontend/src/components/index.ts
@@ -1,11 +1,9 @@
 import {Header} from "./Header/Header"
 import { Card } from "./Card/Card"
 import {Input} from "./Input/Input"
-import {Default} from "./Default/Default"
 import Modal from "./Modal/Modal"
 import { Selected } from "./Select/Selected"
 import {Question} from "./Question/Question"
-import ModalAdd from "./Modal/ModalAdd"
 import { TableUser } from "./Table/Table"
 import {PrivateRoute} from "./PrivateRoute/PrivateRoute"
 
@@ -13,10 +11,8 @@ export {
     Header,
     Input,
     Modal,
-    Default,
     Question,
     Card,
-    ModalAdd,
     Selected,
     TableUser,
     PrivateRoute
