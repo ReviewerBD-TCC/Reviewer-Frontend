41b189b01f319dc532d0b4755815d9200ce78cc2
byankatomaz <byankatomaz@gmail.com>
2024-05-23 11:35:21 -0300

    update Api.ts

--- reviewer-frontend/src/api/Api.ts
+++ reviewer-frontend/src/api/Api.ts
@@ -1,15 +1,53 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
+import { msalInstance } from "../authSSO/msalInstance";
+import { loginRequest } from "../authSSO/authConfig";
+import { AuthenticationResult } from "@azure/msal-common";
 
 export const headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    "Access-Control-Allow-Origin": "true",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  };
-
-  //  export const baseURL =  "http://10.234.88.116:8056/api/v1/"
-  export const baseURL = "http://10.234.81.220:8056/api/v1/"
-
-const api = axios.create({ baseURL, headers: headers })
-export default api  
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  "Access-Control-Allow-Origin": "true",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+};
+ 
+//URL em que fazemos as requisições da API
+export const API_URL = "http://10.234.81.220:8056/api/v1/"
+ 
+ 
+ 
+//Definindo a instância da URL para as requisições utilizando AXIOS
+const instance = axios.create({
+  baseURL: API_URL,
+});
+ 
+ 
+async function acquireToken(
+  config: InternalAxiosRequestConfig
+): Promise<InternalAxiosRequestConfig> {
+  console.log("hii")
+  try {
+    const tokenResponse: AuthenticationResult = await msalInstance.acquireTokenSilent(loginRequest);
+    const token = tokenResponse.idToken;
+    config.headers.Authorization = `Bearer ${token}`
+    return config;
+  } catch (error) {
+    if (error) {
+      // Se ocorrer um erro que requer interação do usuário, redirecione para autenticação interativa
+      console.log("deu erro")
+      await msalInstance.acquireTokenRedirect(loginRequest);
+    } else {
+      // Lida com outros erros de forma apropriada
+      console.error("Erro ao adquirir token:", error);
+      throw error; // Lança o erro para ser tratado pelo chamador
+    }
+ 
+    // Retorna o objeto de configuração original para garantir que o encadeamento da solicitação continue
+    return config;
+  }
+}
+ 
+ 
+instance.interceptors.request.use(acquireToken);
+ 
+export default instance;
