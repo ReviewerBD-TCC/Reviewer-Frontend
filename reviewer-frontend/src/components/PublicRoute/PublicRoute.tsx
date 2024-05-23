702149635664945c18b0f4138a8eae8f62b1b722
byankatomaz <byankatomaz@gmail.com>
2024-05-23 11:36:21 -0300

    update PublicRoute.tsx

--- reviewer-frontend/src/components/PublicRoute/PublicRoute.tsx
+++ reviewer-frontend/src/components/PublicRoute/PublicRoute.tsx
@@ -1,15 +1,13 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "context/AuthProvider";
 import React from "react";
 import { PublicRouteProps } from "interfaces/Geral/PublicRoute";
 
 export const PublicRoute = ({
   children,
 }: PublicRouteProps): React.ReactElement => {
-  const { accessToken } = useAuth();
 
-  if (accessToken) {
-    return <Navigate to="/home" />;
+  if (5 > 0) {
+    return <Navigate to="/login" />;
   }
   return <>{children}</>;
 };
