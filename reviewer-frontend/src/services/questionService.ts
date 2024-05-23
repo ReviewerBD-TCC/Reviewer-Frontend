ab8cfd6cf36d3ff9d80739a7fde83ef9a87f13e4
byankatomaz <byankatomaz@gmail.com>
2024-05-23 11:40:38 -0300

    update questionService.ts

--- reviewer-frontend/src/services/questionService.ts
+++ reviewer-frontend/src/services/questionService.ts
@@ -2,26 +2,18 @@ import { ModalProps } from "interfaces/ModalInterfaces/Modal";
 import api from "../api/Api";
 import { QuestionProps } from "interfaces/QuestionsInterface/Question";
 
-const getQuestions = async (token: string | null) => {
+const getQuestions = async () => {
   try {
-    const response = await api.get('/question', {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-    });
+    const response = await api.get('/question');
     return response.data
   } catch (error) {
     console.error(error)
   }
 }
 
-const postQuestion = async (token: string | null, data: ModalProps) => {
+const postQuestion = async (data: ModalProps) => {
   try {
-    const response = await api.post('question', data, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
+    const response = await api.post('question', data);
     return response
   } catch (error) {
     console.error(error)
@@ -29,13 +21,9 @@ const postQuestion = async (token: string | null, data: ModalProps) => {
 
 }
 
-const updateQuestion = async (token: string | null, data: QuestionProps, id: number) => {
+const updateQuestion = async (data: QuestionProps, id: number) => {
   try {
-    const response = await api.put(`question/${id}`, data, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
+    const response = await api.put(`question/${id}`, data);
     return response
   } catch (err) {
     console.error(err)
@@ -43,18 +31,14 @@ const updateQuestion = async (token: string | null, data: QuestionProps, id: num
 }
 
 
-const updateQuestionActive = async (token: string | null, id: number, active: boolean) => {
+const updateQuestionActive = async (id: number, active: boolean) => {
 
   try {
     const response = await api.patch(
       `question/${id}`,
       {
         active: active,
-      }, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    }
+      }, 
     );
 
     return response.data
