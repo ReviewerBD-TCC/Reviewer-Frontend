c12fb27e7a7f2ce1afbe600b2481070cc08771c7
byankatomaz <byankatomaz@gmail.com>
2024-05-24 09:43:39 -0300

    update app.tsx

--- reviewer-frontend/src/app.tsx
+++ reviewer-frontend/src/app.tsx
@@ -3,16 +3,18 @@ import { msalInstance } from "../src/authSSO/msalInstance";
 // import { AuthProvider } from "context/AuthProvider";
 import { QueryClient, QueryClientProvider } from "react-query";
 import AppRoutes from "routes";
+import { AuthProvider } from "context/AuthProvider";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <MsalProvider instance={msalInstance}>
+      <AuthProvider>
         <QueryClientProvider client={queryClient}>
           <AppRoutes />
         </QueryClientProvider>
-  
+      </AuthProvider>
     </MsalProvider>
   );
 }
