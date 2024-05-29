c563c9c75e903f3d24f09a774118fe108180b433
vicorsi <victorhugocorsi@gmail.com>
2024-05-29 14:40:09 -0300

    fix: Homepage functions

--- reviewer-frontend/src/pages/Homepage/Homepage.tsx
+++ reviewer-frontend/src/pages/Homepage/Homepage.tsx
@@ -1,61 +1,12 @@
-import { Card, Header } from "../../components/index";
+import { Header } from "../../components/index";
 import { useMsal } from "@azure/msal-react";
-import { getCardList } from "components/CardList/CardList";
+import adminHomepage from "./AdminHomepage";
+import userBdHomepage from "./UserBdHomepage";
 
 export const Homepage = () => {
   const { instance } = useMsal();
 
   const account = instance.getActiveAccount();
-
-  const cardList = getCardList()
-
-  const adminHomepage = () => {
-
-    return (
-      <>
-        <div className="w-full h-16 flex">
-          <h1 className="font-bold text-3xl">Painel administrador</h1>
-        </div>
-        <div className="w-full h-2/4 flex justify-around items-center flex-row gap-4 bg-boschWhite">
-          {cardList.map((i: any) => (
-            <Card
-              key={i.key}
-              description={i.description}
-              text={i.title}
-              backgroundColor={i.backgroundColor}
-              nav={i.nav}
-              className={"hover:h-60 shadow-2xl ease-out duration-500"}
-            />
-          ))}
-        </div>
-      </>
-    );
-  };
-
-  const userBdHomepage = () => {
-
-    const filteredCards = cardList.filter((card: any)=> card.key == 6);
-
-    return (
-      <>
-        <div className="w-full h-16 flex">
-          <h1 className="font-bold text-3xl">Painel do usuário</h1>
-        </div>
-        <div className="w-full h-2/4 flex justify-start items-center flex-row gap-4 bg-boschWhite">
-          {filteredCards.map((i: any) => (
-            <Card
-              key={i.key}
-              description={i.description}
-              text={i.title}
-              backgroundColor={i.backgroundColor}
-              nav={i.nav}
-              className={"hover:h-60 shadow-2xl ease-out duration-500"}
-            />
-          ))}
-        </div>
-      </>
-    );
-  };
 
   return (
     <div
@@ -63,11 +14,9 @@ export const Homepage = () => {
     >
       <Header />
       <div className="bg-boschWhite h-screen w-full flex items-center justify-center flex-col">
-        <div className="bg-[#fff] w-[90%] h-full flex flex-col justify-center items-center">
-          {account?.idTokenClaims?.roles?.includes("admina")
+          {account?.idTokenClaims?.roles?.includes("admi")
             ? adminHomepage()
             : userBdHomepage()}
-        </div>
       </div>
     </div>
   );
