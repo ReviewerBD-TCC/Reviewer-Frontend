724b72d5496de3a6b32216de3d6345db78cf0e2f
jzsantana <juliass7187@gmail.com>
2024-05-29 16:43:00 -0300

    feat: new method for get form data

--- reviewer-frontend/src/services/AnswerPerQuestionService.ts
+++ reviewer-frontend/src/services/AnswerPerQuestionService.ts
@@ -1,11 +1,10 @@
-import { AnswerPerQuestionInterface } from "interfaces/QuestionsInterface/AnswerPerQuestion";
 import { api_temp } from "../api/Api";
 
 const getAnswerPerQuestion = async (userId: number, formId: number, questionId: number) => {
     try{
         const response = await api_temp.get(`/answer_form?userId=${userId}&formId=${formId}&questionId=${questionId}`,{
             headers: {
-                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrZXZlbi5zYW50b3NAYm9zY2guY29tIiwiaXNzIjoiQVBJIFJldmlld2VyIiwiaWQiOjIsImV4cCI6MTcxNjkyOTM2N30.O-dP44vSO2XYWxYNyPJpo2THjbG6Zl99YQlgxNbb_gk`
+                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrZXZlbi5zYW50b3NAYm9zY2guY29tIiwiaXNzIjoiQVBJIFJldmlld2VyIiwiaWQiOjEsImV4cCI6MTcxNzAwNzQ0MX0.eV7QjGaW_-_txR8-DviD3N2smmyf-mblUo_ZpdcLUxc`
             }
         })
         return response.data
@@ -14,6 +13,20 @@ const getAnswerPerQuestion = async (userId: number, formId: number, questionId: 
     }
 }
 
+const getAnswerPerForm = async (formId: number) => {
+    try{
+        const response = await api_temp.get(`/answer_form/${formId}`, {
+            headers: {
+                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrZXZlbi5zYW50b3NAYm9zY2guY29tIiwiaXNzIjoiQVBJIFJldmlld2VyIiwiaWQiOjEsImV4cCI6MTcxNzAwNzQ0MX0.eV7QjGaW_-_txR8-DviD3N2smmyf-mblUo_ZpdcLUxc`
+            }
+        })
+        return response.data;
+    }catch(error){
+        console.error(error)
+    }
+}
+
 export const AnswerPerQuestionService = {
-    getAnswerPerQuestion
+    getAnswerPerQuestion,
+    getAnswerPerForm
 }
