74a35311198b0a98875a5ee4dc1c9ccd16142bf5
vicorsi <victorhugocorsi@gmail.com>
2024-06-03 12:36:07 -0300

    fix: Notfound page

--- reviewer-frontend/src/pages/NotFound/Notfound.tsx
+++ reviewer-frontend/src/pages/NotFound/Notfound.tsx
@@ -1,12 +1,13 @@
 import { Header } from 'components';
-import Error from '../../assets/images/404page.gif'
+import Error from '../../assets/images/404.png'
 export const Notfound = () => {
 
     return (
     <div className='w-full min-h-screen h-auto flex flex-col items-center'>
         <Header />
-        <div className='w-full flex-grow flex justify-center items-center'>
+        <div className='w-full flex-grow flex flex-col gap-8 justify-center items-center'>
             <img src={Error} alt="Error Image" className="max-w-full h-auto" />
+            <a href="/" className='text-boschBlue'>Voltar para home </a>
         </div>
     </div>
     );
