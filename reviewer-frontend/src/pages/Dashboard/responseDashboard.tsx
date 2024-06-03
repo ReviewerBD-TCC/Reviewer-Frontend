6c904a6d9f53774bf2ceab6f3c9b237e5215333d
jzsantana <juliass7187@gmail.com>
2024-06-03 14:37:12 -0300

    feat: create method to get data for the graph

--- reviewer-frontend/src/pages/Dashboard/responseDashboard.tsx
+++ reviewer-frontend/src/pages/Dashboard/responseDashboard.tsx
@@ -1,22 +1,19 @@
 import { AnswerPerQuestionInterface } from "interfaces/QuestionsInterface/AnswerPerQuestion";
+import { SparkAccordion, SparkActivityIndicator } from "@bosch-web-dds/spark-ui-react";
 import { AnswerPerQuestionService } from "services/AnswerPerQuestionService";
-import {
-  SparkAccordion,
-  SparkActivityIndicator,
-} from "@bosch-web-dds/spark-ui-react";
+import { QuestionList } from "interfaces/QuestionsInterface/QuestionList";
 import BackButton from "components/BackButton/BackButton";
 import { FormService } from "services/FormService";
 import { useAuth } from "context/AuthProvider";
-import { useQuery } from "react-query";
-import { Header, Question } from "components";
-import React, { useEffect, useState } from "react";
-import { QuestionList } from "interfaces/QuestionsInterface/QuestionList";
+import { useEffect, useState } from "react";
 import Graphic from "components/Chart/Chart";
+import { useQuery } from "react-query";
+import { Header } from "components";
 
 function ResponseDashboard() {
   // const { accessToken } = useAuth();
-  // const [openAccordionId, setOpenAccordionId] = useState<number | null>(null);
   const [questions, setQuestions] = useState<number>(1);
+  const { dashboard } = useAuth();
 
   const { data: responseFormQuestionList = [], isLoading } = useQuery(
     "form",
@@ -29,22 +26,50 @@ function ResponseDashboard() {
     return AnswerPerQuestionService.getAnswerPerForm(1);
   });
 
-  const chartData:any [] = []
+  const chartData: any[] = [];
+
+  const graphResponse = (id: number) => {
+    if (dashboard) {
+      console.log("dentro da função", dashboard[0]);
+
+      const array = dashboard.find((subArray: any) => {
+        let found = false;
+        subArray.forEach((obj: any) => {
+          if (obj.id === id) {
+            found = true;
+          }
+        });
+        return found;
+      });
+
+      if (Array.isArray(array)) {
+        const object = array.find((obj: any) => obj.id === id);
+
+        if (object) {
+          const quantityAnsweredForm = object.quantityAnsweredForm;
+          const quantityFormSent = object.quantityFormSent;
+
+          chartData.push(quantityFormSent * 20, quantityAnsweredForm * 20)
+
+          console.log("char", chartData);
+          console.log(quantityAnsweredForm);
+          console.log(quantityFormSent);
+        }
+        return chartData
+      }
+    } else {
+      console.log("erro");
+    }
+    return chartData
+  };
+
+  console.log('teste do chart',chartData)
 
   useEffect(() => {
-    if (responseAnswerList) {
-     const quantityResp = responseAnswerList.filter((answer: AnswerPerQuestionInterface) =>
-      answer.quantityFormSent >= 0)
+    graphResponse(1);
+    console.log("useEffect ", dashboard);
+  }, [questions, chartData]);
 
-      const quantityAnsweredForm = responseAnswerList.filter((answer: AnswerPerQuestionInterface) => 
-      answer.quantityAnsweredForm >= 0)
-      
-      chartData.push(quantityResp, quantityAnsweredForm)
-    }
-    else{
-      console.log("Errrrrrrou")
-    }
-  }, [questions]);
 
   return (
     <div className="w-full min-h-screen h-auto flex flex-col items-center">
@@ -52,14 +77,14 @@ function ResponseDashboard() {
       <div className="bg-boschWhite w-full min-h-[90%] h-auto flex items-center justify-center pt-7">
         <div className=" w-[90%] h-auto flex flex-col justify-center items-center gap-6 pt-7 pb-7">
           <div className="w-full justify-start items-start">
-            <BackButton navigateTo="/" />
+            <BackButton navigateTo="/"/>
           </div>
           <div className="w-full h-auto flex flex-col gap-2">
             <h1 className="font-bold text-3xl">
               Formulário de responsabilidade social
             </h1>
             <p>
-              Estes são feedbacks referentes a: 
+              Estes são feedbacks referentes a:
               <span className="font-extrabold text-boschBlue pl-1">
                 Vitor Alves Santos
               </span>
@@ -69,7 +94,11 @@ function ResponseDashboard() {
 
           <div className="w-auto h-auto max-h-44 self-start flex flex-col items-center justify-center gap-1 p-1">
             <p className="font-bold text-lg">Taxa de adesão</p>
-            <Graphic data={chartData}/>
+            {chartData ? (
+              <Graphic data={chartData} />
+            ) : (
+              <p>Gráfico indisponível</p>
+            )}
           </div>
 
           <div className="w-full pt-4">
