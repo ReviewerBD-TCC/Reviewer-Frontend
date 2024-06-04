19bd0bed4f1ce344f9c32fc1bae6e6fe1a9605f4
jzsantana <juliass7187@gmail.com>
2024-06-04 16:57:28 -0300

    fix: remove context

--- reviewer-frontend/src/pages/Homepage/Homepage.tsx
+++ reviewer-frontend/src/pages/Homepage/Homepage.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import { Card, Header } from '../../components/index'
-import { AnswerPerQuestionService } from 'services/AnswerPerQuestionService';
-import { useQuery } from 'react-query';
-import { useAuth } from 'context/AuthProvider';
+import { Header } from "../../components/index";
 import { useMsal } from "@azure/msal-react";
 import adminHomepage from "./AdminHomepage";
 import userBdHomepage from "./UserBdHomepage";
 
 export const Homepage = () => {
-
-    const { data: responseAnswerList = [] } = useQuery("answer", () => {
-      return AnswerPerQuestionService.getAnswerPerForm(1);
-    });
-
-    const { setDashboard } = useAuth();
-
-    setDashboard(responseAnswerList); 
-
-    const { instance } = useMsal();
- 
-    const account = instance.getActiveAccount();
-
+  const { instance } = useMsal();
+  const account = instance.getActiveAccount();
+  console.log(account?.idToken)
   return (
     <div
       className={`bg-[#fff] w-full h-screen flex justify-center items-center flex-col`}
     >
       <Header />
       <div className="bg-boschWhite h-screen w-full flex items-center justify-center flex-col">
-          {account?.idTokenClaims?.roles?.includes("User.Read")
-            ? adminHomepage()
-            : userBdHomepage()}
+        {account?.idTokenClaims?.roles?.includes("User.Read")
+          ? adminHomepage()
+          : userBdHomepage()}
       </div>
     </div>
   );
