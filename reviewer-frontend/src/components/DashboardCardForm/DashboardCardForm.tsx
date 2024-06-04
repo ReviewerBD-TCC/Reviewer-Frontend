f565dcd952c56b9a255e760d86a0013aaf91b838
vicorsi <victorhugocorsi@gmail.com>
2024-06-04 16:52:58 -0300

    fix: Dashboard Card

--- reviewer-frontend/src/components/DashboardCardForm/DashboardCardForm.tsx
+++ reviewer-frontend/src/components/DashboardCardForm/DashboardCardForm.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { cardFormProps } from 'interfaces/FormInterfaces/CardForm'
-
-const DashboardCardForm: React.FC<cardFormProps> = ({ titleForm, id, className, onClick }: cardFormProps) => {
-    return (
-    <div onClick={onClick}>
-        <div className={`bg-boschLightGray w-full min-h-[3.8rem] p-4 flex-row flex items-center ${className}`}>
-            <h2 className='font-bold text-xl'>
-                {titleForm}
-            </h2>
+import { Link } from 'react-router-dom'
+ 
+const DashboardCardForm: React.FC<cardFormProps> = ({titleForm, id, className, onClick, subTitle, linkNav}: cardFormProps) => {
+  return (
+    <div>
+      <div onClick={onClick}  className={`bg-boschLightGray w-full min-h-[3.8rem] p-4 flex-col flex  ${className}`}>
+        <h2 className='font-bold text-xl'>
+            {titleForm}
+        </h2>
+        {
+          subTitle ? (
+          <h2>
+          <span>Indicado por:</span> <span className='text-boschBlue text-base font-bold'> {subTitle} </span>
+        </h2>) : <p></p>
+        }
       </div>
     </div>
   )
 }
-
+ 
 export default DashboardCardForm
