110623192805018be701d7573a8ae117a11d8109
vicorsi <victorhugocorsi@gmail.com>
2024-06-04 09:59:15 -0300

    fix: userBd homepage

--- reviewer-frontend/src/pages/Homepage/UserBdHomepage.tsx
+++ reviewer-frontend/src/pages/Homepage/UserBdHomepage.tsx
@@ -22,11 +22,15 @@ const userBdHomepage = () => {
     const getAnswersByUserId = async () => {
         const data = await AnswerService.getAnswerByUserId(userId)
         setFormList(data)
-        console.log(formList)
     }
+    
+    // const getAnswersFormPending = async () => {
+    //   const data = await AnswerService.getAnswersFormPending(userId)
+    //   console.log(data)
+    // }
 
     return (
-    <div className="bg-[#fff] w-[90%] h-full flex flex-col items-center pt-14 ">
+    <div className="bg-[#fff] w-[90%] h-full flex flex-col items-center pt-14 gap-2">
         <div className="w-full h-16 flex">
           <h1 className="font-bold text-3xl">Painel do usuário</h1>
         </div>
@@ -36,14 +40,18 @@ const userBdHomepage = () => {
             onClick={()=>getAnswersByUserId()}
             />         
         </div>
-        <div>
-            {/* {
-                tabValue == "1" ? formList.map((i:FormInterface, index: number)=>(
-                    <DashboardCardForm key={index} id={i.id} titleForm={i.title}
-                ))
-            } */}
+        <div className="flex flex-col gap-2 w-full">
+          {
+            tabValue == "2" ? (
+              [...new Map(formList.map(item => [item.whichUserName, item])).values()]
+              .map((i: FormInterface, index: number) => (
+                  <DashboardCardForm key={i.id} id={i.id} titleForm={i.whichUserName} />
+              ))
+          ) : null
+          }
         </div>
-      </div>
+
+    </div>
     );
   };
 
