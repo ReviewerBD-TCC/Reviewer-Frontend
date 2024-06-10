b13039a63054820310814747b6f4a8d8c287229c
vicorsi <victorhugocorsi@gmail.com>
2024-06-10 13:20:07 -0300

    fix: Form route

--- reviewer-frontend/src/routes/index.tsx
+++ reviewer-frontend/src/routes/index.tsx
@@ -73,7 +73,7 @@ export const route = createBrowserRouter([
     )
   },
   {
-    path: "/form/:formId/",
+    path: "/form/:formId/:userId/",
     element: (
       <PrivateRoute>
         <Form />
